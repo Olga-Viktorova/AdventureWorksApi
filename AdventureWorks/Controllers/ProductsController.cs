89de77362f6f211fd28ae8b95ef6d56ed6c4cdb1
Volha Viktarava <volha_viktarava@epam.com>
2018-10-27 22:12:41 +0300

    Add AdventureWorksApi repository

---
+++ AdventureWorks/Controllers/ProductsController.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdventureWorksApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace AdventureWorksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AdventureWorksDbContext _context;
+        private readonly ILogger _logger;
+
+        public ProductsController(AdventureWorksDbContext context, ILogger<ProductsController> logger)
+        {
+          _context = context;
+          _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProduct()
+        {
+            return _context.Product;
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError("Product Not Found, Bad request");
+                return BadRequest(ModelState);
+            }
+
+            var product = await _context.Product.FindAsync(id);
+
+            if (product == null)
+            {
+                _logger.LogInformation("Product Not Found");
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct([FromRoute] int id, [FromBody] Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != product.ProductId)
+            {
+                _logger.LogError("Product Not Found, Bad request");
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                     _logger.LogInformation("Product Not Found");
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            _logger.LogInformation("Not Content");
+            return NoContent();
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public async Task<IActionResult> PostProduct([FromBody] Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError("Product Not Found, Bad request");
+                return BadRequest(ModelState);
+            }
+
+            _context.Product.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduct", new { id = product.ProductId }, product);
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError("Product Not Found, Bad request");
+                return BadRequest(ModelState);
+            }
+
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
+            {
+              _logger.LogInformation("Product Not Found");
+              return NotFound();
+            }
+
+            _context.Product.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Product.Any(e => e.ProductId == id);
+        }
+    }
+}
---
+++ AdventureWorks/Startup.cs
@@ -0,0 +1,66 @@
+﻿using AdventureWorksApi.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace AdventureWorksApi
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+      Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+
+      var connection = @"Server=mysqlvmlabel.westeurope.cloudapp.azure.com,1433;Database=AdventureWorks2012;Trusted_Connection=True;ConnectRetryCount=0";
+      services.AddDbContext<AdventureWorksDbContext>(options => options.UseSqlServer(connection));
+
+      services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Info {Title = "My API", Version = "v1"}));
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+      }
+
+      app.UseStaticFiles();
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+          name: "default",
+          template: "{controller=Home}/{action=Index}/{id?}");
+      });
+
+      loggerFactory.AddSerilog();
+    }
+
+    public static IWebHost BuildWebHost(string[] args) =>
+      WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().UseSerilog().Build();
+  }
+}
